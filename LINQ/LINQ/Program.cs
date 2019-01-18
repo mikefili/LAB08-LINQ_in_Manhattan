6c355dd90e65c738139b018801de56715f319f0e
Michael Filicetti <filicetti.michael@gmail.com>
2019-01-17 21:24:32 -0800

    broke out GetJObject method for filter methods

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -8,20 +8,22 @@ namespace LINQ
 {
     public class Program
     {
-
+        public static string dataPath = "C:/Users/Owner/codefellows/401/labs/Lab08-LINQ_In_Manhattan/data.json";
 
         static void Main(string[] args)
         {
-            string dataPath = "C:/Users/Owner/codefellows/401/labs/Lab08-LINQ_In_Manhattan/data.json";
-            HandleJSON(dataPath);
+            
+            HandleJSON(GetJObject(dataPath));
         }
 
-        
-
-        public static JObject HandleJSON(string path)
+        public static JObject GetJObject(string path)
         {
             JObject jObject = JObject.Parse(File.ReadAllText(@path));
+            return jObject;
+        }
 
+        public static JObject HandleJSON(JObject jObject)
+        {
             JArray items = (JArray)jObject["features"];
             int dataArr = items.Count;
 
@@ -48,6 +50,13 @@ namespace LINQ
             return jObject;
         }
 
-        
+        //static IEnumerable<string> filterWithoutNeighborhood(IEnumerable<string> items)
+        //{
+        //    HandleJSON();
+        //    // anonymous type variable
+        //    var filterWithoutNeighborhood = from  in bestCats
+        //                       where cats.Contains("e")
+        //                       select cats;
+        //}
     }
 }
